4a39774158f088483cdd4b9beef6cf1418f1bbd1
felipersan <feliperodrigues.san@gmail.com>
2022-11-22 17:05:55 -0300

    update files

--- src/pages/teste/index.tsx
+++ src/pages/teste/index.tsx
@@ -6,7 +6,7 @@ export default function Teste() {
     getObject();
   }, []);
   async function getObject() {
-    await API.get(`pokemon?limit=13410&offset=0`).then((response: any) => {
+    await API.get(`pokemon?limit=4000&offset=0`).then((response: any) => {
       let paths:any = [];
       response.data.results.map((row: any) => {
         let newObject = {
