a42845b26272a8a7dc2dc972af45a8670eb7732c
felipersan <feliperodrigues.san@gmail.com>
2022-11-22 12:46:36 -0300

    implement form on input component

--- src/components/UI/InputDefault/index.tsx
+++ src/components/UI/InputDefault/index.tsx
@@ -1,22 +1,21 @@
 import { InputHTMLAttributes, ReactNode } from "react";
-import {SearchIcon} from "../../../../public/assets/images/icons";
+import { SearchIcon } from "../../../../public/assets/images/icons";
 import { Container, Input } from "./styles";
 
 interface buttonProps extends InputHTMLAttributes<HTMLInputElement> {
-  search: ()=>void
-  icon: ReactNode
+  search: () => void;
+  icon: ReactNode;
 }
 
 export default function InputDefault({ search, icon, ...props }: buttonProps) {
   return (
+      <form onSubmit={search}>
     <Container>
-      <Input {...props} />
-      <button
-      className="searchIconForInput"
-      onClick={search}
-      >
-        {icon}
-      </button>
+        <Input {...props} />
+        <button className="searchIconForInput" onClick={()=>{SubmitEvent}}>
+          {icon}
+        </button>
     </Container>
+      </form>
   );
 }
