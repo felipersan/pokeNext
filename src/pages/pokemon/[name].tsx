983abcb63392611533d544060343cdd1c95ad687
felipersan <feliperodrigues.san@gmail.com>
2022-11-22 12:40:09 -0300

    update files

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -1,73 +1,92 @@
-import { AnyARecord } from 'dns'
-import Image from 'next/image'
-import EvolutionChain from '../../components/pages/pokemon/EvolutionChain'
-import ImageCard from '../../components/pages/pokemon/ImageCard'
-import PrincipalInformation from '../../components/pages/pokemon/PrincipalInformation'
-import RadarPokemonChart from '../../components/pages/pokemon/RadarChart'
-import { searchPokemon } from '../../interfaces/API/GET'
-import API from '../../services/pokeAPI'
+import { AnyARecord } from "dns";
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import EvolutionChain from "../../components/pages/pokemon/EvolutionChain";
+import ImageCard from "../../components/pages/pokemon/ImageCard";
+import PrincipalInformation from "../../components/pages/pokemon/PrincipalInformation";
+import RadarPokemonChart from "../../components/pages/pokemon/RadarChart";
+import { searchPokemon } from "../../interfaces/API/GET";
+import API from "../../services/pokeAPI";
+import styles from "../../styles/Home.module.css";
 
-import { Container } from '../../styles/pages/pokemon/name/styles'
+import { Container } from "../../styles/pages/pokemon/name/styles";
 
 export async function getStaticProps(context: any) {
-  const { params } = context
+  const { params } = context;
   const pokemon: searchPokemon = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${params.name}`
   ).then((response: any) => {
-    return response.json()
-  })
+    return response.json();
+  });
   const species: any = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${params.name}`
   ).then((response: any) => {
-    return response.json()
-  })
+    return response.json();
+  });
 
   return {
     props: {
       pokemon,
-      species
-    }
-  }
+      species,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  let paths: any = []
+  let paths: any = [];
 
   await API.get(`pokemon?limit=200&offset=0`).then((response: any) => {
     response.data.results.map((row: any, key: number) => {
-      if (row !== 'voltorb') {
+      if (row !== "voltorb") {
         let newObject = {
           params: {
-            name: row?.name
-          }
-        }
-        paths.push(newObject)
+            name: row?.name,
+          },
+        };
+        paths.push(newObject);
       }
-    })
-  })
+    });
+  });
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export default function pokemonName({ pokemon, species }: any) {
+const Pokemon: NextPage = ({ pokemon, species }: any) => {
+  const router = useRouter();
+  const { name } = router.query;
   return (
-    <Container>
-      <div className="letSidePokemonDetails">
-        <ImageCard pokemon={pokemon} />
-      </div>
-      <div className="rightSidePokemonDetails">
-        <PrincipalInformation pokemon={pokemon} />
-        <div className="bottomSizeWithChart">
-          <div className="radarChartPokemon">
-            <RadarPokemonChart pokemon={pokemon} />
-          </div>
-          <div className="evolutionChain">
-            <EvolutionChain pokemon={pokemon} species={species} />
+    <div className={styles.container}>
+      <Head>
+        <title>Pokemon - {name}</title>
+        <meta
+          name="description"
+          content={`Página do pokemon - ${name}`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <div className="letSidePokemonDetails">
+          <ImageCard pokemon={pokemon} />
+        </div>
+        <div className="rightSidePokemonDetails">
+          <PrincipalInformation pokemon={pokemon} />
+          <div className="bottomSizeWithChart">
+            <div className="radarChartPokemon">
+              <RadarPokemonChart pokemon={pokemon} />
+            </div>
+            <div className="evolutionChain">
+              <EvolutionChain pokemon={pokemon} species={species} />
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-  )
+      </Container>
+    </div>
+  );
 }
+
+export default Pokemon;
+
